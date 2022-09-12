50c3c661cfd65cedc6fa8116bd58fcc5bbd7297c
MShahryiar <muhammadshahryiar@yahoo.com>
2022-09-12 17:36:29 +0500

    styling the comments

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -11,7 +11,7 @@ interface Props{
 function Feed({tweets}:Props) {
   // console.log(tweets)
   return (
-    <div className='col-span-7 lg:col-span-5 border-x'>
+    <div className='col-span-7 lg:col-span-5 border-x '>
         <div className='flex items-center  justify-between'>
             <h1 className='p-5 pb-0 text-xl font-bold'>Home</h1>
             <RefreshIcon className='h-8 w-8 mr-5 mt-5 text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125'/>
